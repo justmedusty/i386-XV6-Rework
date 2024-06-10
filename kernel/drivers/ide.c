a962fb8ed710190064c06af8b90b8ecdda0f44e0
dustyn <dustyn_gibb@hotmail.com>
2024-06-10 05:49:31 +0000

    FUNCTIONAL: Huge additions to the ide driver. Need to fix iderw

--- kernel/drivers/ide.c
+++ kernel/drivers/ide.c
@@ -52,11 +52,24 @@
 
 static struct spinlock idelock;
 static struct spinlock idelock2;
+static struct spinlock idelock3;
+static struct spinlock idelock4;
+static struct spinlock idelock5;
+static struct spinlock idelock6;
+static struct spinlock idelock7;
+static struct spinlock idelock8;
+
 static struct buf *idequeue;
 static struct buf *idequeue2;
+static struct buf *idequeue3;
+static struct buf *idequeue4;
+static struct buf *idequeue5;
+static struct buf *idequeue6;
+static struct buf *idequeue7;
+static struct buf *idequeue8;
 
 unsigned char disk_presence;
-
+unsigned char startup;
 
 // Wait for IDE disk to become ready.
 static void idestart(uint dev, struct buf *);
@@ -64,7 +77,7 @@ static void idestart(uint dev, struct buf *);
 /*
  * Simple function that returns a char bitmask indicating which disks are present, will be useful for santizing random device numbers passed as parameters.
  */
-char disk_query(){
+char disk_query() {
     return disk_presence;
 }
 
@@ -77,17 +90,42 @@ static int idewait(int dev, int checkerr) {
      * I have to send the ident cmd to the secondary ata controller for some reason, I did not have to do this
      * for the first disk. I am not sure why. But anyway, this is why this is here.
      */
-    if (!(disk_presence & DISK3) || !((disk_presence & DISK4)) || !((disk_presence & DISK5)) || !((disk_presence & DISK6))|| !((disk_presence & DISK7)) || !((disk_presence & DISK8))) {
+    if (!(startup & DISK3)) {
+        //only do this once, mask it after
         outb(BASEPORT2 + 6, 0xe0 | (0 << 4));
+        startup |= DISK3;
     }
+    if (!(disk_presence & DISK4)) {
+        outb(BASEPORT2 + 6, 0xe0 | (1 << 4));
+        startup |= DISK4;
+    }
+    if (!(disk_presence & DISK5)) {
+        outb(BASEPORT3 + 6, 0xe0 | (0 << 4));
+        startup |= DISK5;
+    }
+    if (!(disk_presence & DISK6)) {
+        outb(BASEPORT3 + 6, 0xe0 | (1 << 4));
+        startup |= DISK6;
+    }
+    if (!(disk_presence & DISK7)) {
+        outb(BASEPORT4 + 6, 0xe0 | (0 << 4));
+        startup |= DISK7;
+    }
+    if (!(disk_presence & DISK8)) {
+        outb(BASEPORT4 + 6, 0xe0 | (1 << 4));
+        startup |= DISK8;
+    }
+
 
     while (((r = inb(port)) & (IDE_BSY | IDE_DRDY)) != IDE_DRDY) {
 
         if (r & 0xff) {
             cprintf("Error on port : %x on dev: %x\n", inb(port), dev);
+            return -1;
         }
         if (r == 0) {
             cprintf("Disk not found on dev: %x on port: %x\n", dev, port);
+            return -1;
         }
         if (checkerr && (r & (IDE_DF | IDE_ERR)) != 0) {
             return -1;
@@ -109,7 +147,7 @@ ideinit(void) {
     //Check if disk 0 (ata0 master) is present
     outb(BASEPORT1 + 6, 0xe0 | (0 << 4));
     for (i = 0; i < 1000; i++) {
-        if (inb(BASEPORT1 + 7) != 0) {
+        if (inb(BASEPORT1 + 7) != 0 && inb(BASEPORT1 + 7) != 0xff) {
             disk_presence |= DISK1;
             cprintf("Found disk 0 master : %x\n", inb(BASEPORT1 + 7));
             break;
@@ -119,7 +157,7 @@ ideinit(void) {
     // Check if disk 1 (ata0 slave) is present
     outb(BASEPORT1 + 6, 0xe0 | (1 << 4));
     for (i = 0; i < 1000; i++) {
-        if (inb(BASEPORT1 + 7) != 0) {
+        if (inb(BASEPORT1 + 7) != 0 && (inb(BASEPORT1 + 7) != 0xff)) {
             disk_presence |= DISK2;
             cprintf("Found disk 0 slave : %x\n", inb(BASEPORT1 + 7));
             break;
@@ -130,7 +168,7 @@ ideinit(void) {
     // Check if disk 2 (ata1 master) is present
     outb(BASEPORT2 + 6, 0xe0 | (0 << 4));
     for (i = 0; i < 1000; i++) {
-        if (inb(BASEPORT2 + 7) != 0) {
+        if (inb(BASEPORT2 + 7) != 0 && (inb(BASEPORT2 + 7) != 0xff)) {
             disk_presence |= DISK3;
             cprintf("Found disk 1 master : %x\n", inb(BASEPORT2 + 7));
             break;
@@ -140,7 +178,7 @@ ideinit(void) {
     // Check if disk 3 (ata1 slave) is present
     outb(BASEPORT2 + 6, 0xe0 | (1 << 4));
     for (i = 0; i < 1000; i++) {
-        if (inb(BASEPORT2 + 7) != 0) {
+        if (inb(BASEPORT2 + 7) != 0 && inb(BASEPORT2 + 7) != 0xff) {
             disk_presence |= DISK4;
             cprintf("Found disk 1 slave : %x\n", inb(BASEPORT2 + 7));
             break;
@@ -150,7 +188,7 @@ ideinit(void) {
     // Check if disk 4 (ata2 master) is present
     outb(BASEPORT3 + 6, 0xe0 | (0 << 4));
     for (i = 0; i < 1000; i++) {
-        if (inb(BASEPORT3 + 7) != 0) {
+        if (inb(BASEPORT3 + 7) != 0 && inb(BASEPORT3 + 7) != 0xff) {
             disk_presence |= DISK5;
             cprintf("Found disk 2 master : %x\n", inb(BASEPORT3 + 7));
             break;
@@ -160,9 +198,9 @@ ideinit(void) {
     // Check if disk 5 (ata2 slave) is present
     outb(BASEPORT3 + 6, 0xe0 | (1 << 4));
     for (i = 0; i < 1000; i++) {
-        if (inb(BASEPORT3 + 7) != 0) {
+        if (inb(BASEPORT3 + 7) != 0 && inb(BASEPORT3 + 7) != 0xff) {
             disk_presence |= DISK6;
-            cprintf("Found disk 2 master : %x\n", inb(BASEPORT3 + 7));
+            cprintf("Found disk 2 slave : %x\n", inb(BASEPORT3 + 7));
             break;
         }
     }
@@ -170,9 +208,9 @@ ideinit(void) {
     // Check if disk 6 (ata3 master) is present
     outb(BASEPORT4 + 6, 0xe0 | (0 << 4));
     for (i = 0; i < 1000; i++) {
-        if (inb(BASEPORT4 + 7) != 0) {
+        if (inb(BASEPORT4 + 7) != 0 && inb(BASEPORT4 + 7) != 0xff) {
             disk_presence |= DISK7;
-            cprintf("Found disk 2 master : %x\n", inb(BASEPORT4 + 7));
+            cprintf("Found disk 3 master : %x\n", inb(BASEPORT4 + 7));
             break;
         }
     }
@@ -180,9 +218,9 @@ ideinit(void) {
     // Check if disk 6 (ata3 master) is present
     outb(BASEPORT4 + 6, 0xe0 | (1 << 4));
     for (i = 0; i < 1000; i++) {
-        if (inb(BASEPORT4 + 7) != 0) {
+        if ((inb(BASEPORT4 + 7) != 0) && inb(BASEPORT4 + 7) != 0xff) {
             disk_presence |= DISK8;
-            cprintf("Found disk 2 master : %x\n", inb(BASEPORT4 + 7));
+            cprintf("Found disk 3 slave : %x\n", inb(BASEPORT4 + 7));
             break;
         }
     }
@@ -195,10 +233,10 @@ ideinit(void) {
     if (!(disk_presence & DISK1)) {
         panic("Missing disk 1");
     }
-    if (!(disk_presence & DISK2))  {
+    if (!(disk_presence & DISK2)) {
         panic("Missing disk 2");
     }
-    if (!(disk_presence & DISK3))  {
+    if (!(disk_presence & DISK3)) {
         panic("Missing disk 3");
     }
 
@@ -219,37 +257,114 @@ idestart(uint dev, struct buf *b) {
 
     if (sector_per_block > 7) panic("idestart");
 
+    switch (dev) {
+        case 1:
+            idewait(1, 0);
+            outb(CONTROLBASE1, 0);  // generate interrupt
+            outb(BASEPORT1 + 2, sector_per_block);  // number of sectors
+            outb(BASEPORT1 + 3, sector & 0xff);
+            outb(BASEPORT1 + 4, (sector >> 8) & 0xff);
+            outb(BASEPORT1 + 5, (sector >> 16) & 0xff);
+            outb(BASEPORT1 + 6, 0xe0 | ((b->dev & 1) << 4) | ((sector >> 24) & 0x0f));
+            if (b->flags & B_DIRTY) {
+                outb(BASEPORT1 + 7, write_cmd);
+                outsl(BASEPORT1, b->data, BSIZE / 4);
+            } else {
+                outb(BASEPORT1 + 7, read_cmd);
+            }
+            break;
+        case 2:
+            idewait(2, 0);
+            outb(CONTROLBASE2, 0);  // generate interrupt
+            outb(BASEPORT2 + 2, sector_per_block);  // number of sectors
+            outb(BASEPORT2 + 3, sector & 0xff);
+            outb(BASEPORT2 + 4, (sector >> 8) & 0xff);
+            outb(BASEPORT2 + 5, (sector >> 16) & 0xff);
+            outb(BASEPORT2 + 6, 0xe0 | (b->dev << 4) | ((sector >> 24) & 0x0f));
+            if (b->flags & B_DIRTY) {
+                outb(BASEPORT2 + 7, write_cmd);
+                outsl(BASEPORT2, b->data, BSIZE / 4);
+            } else {
+                outb(BASEPORT2 + 7, read_cmd);
+            }
+            break;
 
-    if (dev == 1) {
-        idewait(1, 0);
-        outb(CONTROLBASE1, 0);  // generate interrupt
-        outb(BASEPORT1 + 2, sector_per_block);  // number of sectors
-        outb(BASEPORT1 + 3, sector & 0xff);
-        outb(BASEPORT1 + 4, (sector >> 8) & 0xff);
-        outb(BASEPORT1 + 5, (sector >> 16) & 0xff);
-        outb(BASEPORT1 + 6, 0xe0 | ((b->dev & 1) << 4) | ((sector >> 24) & 0x0f));
-        if (b->flags & B_DIRTY) {
-            outb(BASEPORT1 + 7, write_cmd);
-            outsl(BASEPORT1, b->data, BSIZE / 4);
-        } else {
-            outb(BASEPORT1 + 7, read_cmd);
-        }
+        case 3:
+            idewait(3, 0);
+            outb(CONTROLBASE2, 0);  // generate interrupt
+            outb(BASEPORT2 + 2, sector_per_block);  // number of sectors
+            outb(BASEPORT2 + 3, sector & 0xff);
+            outb(BASEPORT2 + 4, (sector >> 8) & 0xff);
+            outb(BASEPORT2 + 5, (sector >> 16) & 0xff);
+            outb(BASEPORT2 + 6, 0xe0 | (b->dev << 4) | ((sector >> 24) & 0x0f));
+            if (b->flags & B_DIRTY) {
+                outb(BASEPORT2 + 7, write_cmd);
+                outsl(BASEPORT2, b->data, BSIZE / 4);
+            } else {
+                outb(BASEPORT2 + 7, read_cmd);
+            }
+            break;
 
-    } else if (dev == 2) {
-        idewait(2, 0);
-        outb(CONTROLBASE2, 0);  // generate interrupt
-        outb(BASEPORT2 + 2, sector_per_block);  // number of sectors
-        outb(BASEPORT2 + 3, sector & 0xff);
-        outb(BASEPORT2 + 4, (sector >> 8) & 0xff);
-        outb(BASEPORT2 + 5, (sector >> 16) & 0xff);
-        outb(BASEPORT2 + 6, 0xe0 | (0 << 4) | ((sector >> 24) & 0x0f));
-        if (b->flags & B_DIRTY) {
-            outb(BASEPORT2 + 7, write_cmd);
-            outsl(BASEPORT2, b->data, BSIZE / 4);
-        } else {
-            outb(BASEPORT2 + 7, read_cmd);
-
-        }
+        case 4:
+            idewait(4, 0);
+            outb(CONTROLBASE3, 0);  // generate interrupt
+            outb(BASEPORT3 + 2, sector_per_block);  // number of sectors
+            outb(BASEPORT3 + 3, sector & 0xff);
+            outb(BASEPORT3 + 4, (sector >> 8) & 0xff);
+            outb(BASEPORT3 + 5, (sector >> 16) & 0xff);
+            outb(BASEPORT3 + 6, 0xe0 | (b->dev << 4) | ((sector >> 24) & 0x0f));
+            if (b->flags & B_DIRTY) {
+                outb(BASEPORT3 + 7, write_cmd);
+                outsl(BASEPORT3, b->data, BSIZE / 4);
+            } else {
+                outb(BASEPORT3 + 7, read_cmd);
+            }
+            break;
+        case 5:
+            idewait(5, 0);
+            outb(CONTROLBASE3, 0);  // generate interrupt
+            outb(BASEPORT3 + 2, sector_per_block);  // number of sectors
+            outb(BASEPORT3 + 3, sector & 0xff);
+            outb(BASEPORT3 + 4, (sector >> 8) & 0xff);
+            outb(BASEPORT3 + 5, (sector >> 16) & 0xff);
+            outb(BASEPORT3 + 6, 0xe0 | (b->dev << 4) | ((sector >> 24) & 0x0f));
+            if (b->flags & B_DIRTY) {
+                outb(BASEPORT3 + 7, write_cmd);
+                outsl(BASEPORT3, b->data, BSIZE / 4);
+            } else {
+                outb(BASEPORT3 + 7, read_cmd);
+            }
+            break;
+        case 6:
+            idewait(6, 0);
+            outb(CONTROLBASE4, 0);  // generate interrupt
+            outb(BASEPORT4 + 2, sector_per_block);  // number of sectors
+            outb(BASEPORT4 + 3, sector & 0xff);
+            outb(BASEPORT4 + 4, (sector >> 8) & 0xff);
+            outb(BASEPORT4 + 5, (sector >> 16) & 0xff);
+            outb(BASEPORT4 + 6, 0xe0 | (b->dev << 4) | ((sector >> 24) & 0x0f));
+            if (b->flags & B_DIRTY) {
+                outb(BASEPORT4 + 7, write_cmd);
+                outsl(BASEPORT4, b->data, BSIZE / 4);
+            } else {
+                outb(BASEPORT4 + 7, read_cmd);
+            }
+            break;
+        case 7:
+            idewait(6, 0);
+            outb(CONTROLBASE4, 0);  // generate interrupt
+            outb(BASEPORT4 + 2, sector_per_block);  // number of sectors
+            outb(BASEPORT4 + 3, sector & 0xff);
+            outb(BASEPORT4 + 4, (sector >> 8) & 0xff);
+            outb(BASEPORT4 + 5, (sector >> 16) & 0xff);
+            outb(BASEPORT4 + 6, 0xe0 | (b->dev << 4) | ((sector >> 24) & 0x0f));
+            if (b->flags & B_DIRTY) {
+                outb(BASEPORT4 + 7, write_cmd);
+                outsl(BASEPORT4, b->data, BSIZE / 4);
+            } else {
+                outb(BASEPORT4 + 7, read_cmd);
+            }
+            break;
     }
 
 }
@@ -259,36 +374,26 @@ idestart(uint dev, struct buf *b) {
 void
 ideintr(void) {
     struct buf *b;
-
     // First queued buffer is the active request.
     acquire(&idelock);
-
     if ((b = idequeue) == 0) {
         release(&idelock);
         return;
     }
     idequeue = b->qnext;
-
     // Read data if needed.
     if (!(b->flags & B_DIRTY) && idewait(b->dev, 1) >= 0) {
         int baseport = BASEPORT1;
         insl(baseport, b->data, BSIZE / 4);
-
     }
-
     // Wake process waiting for this buf.
     b->flags |= B_VALID;
     b->flags &= ~B_DIRTY;
     wakeup(b);
-
-
     if ((b = idequeue) != 0) {
         idestart(b->dev, idequeue);
-
         idequeue = b->qnext;
-
         release(&idelock);
-
     }
     release(&idelock);
 }
@@ -299,37 +404,25 @@ ideintr2(void) {
 
     // First queued buffer is the active request.
     acquire(&idelock2);
-
-
     if ((b = idequeue2) == 0) {
         release(&idelock);
         return;
     }
     idequeue2 = b->qnext;
-
     // Read data if needed.
     if (!(b->flags & B_DIRTY) && idewait(2, 1) >= 0) {
         int baseport = BASEPORT2;
         insl(baseport, b->data, BSIZE / 4);
-
     }
-
     // Wake process waiting for this buf.
     b->flags |= B_VALID;
     b->flags &= ~B_DIRTY;
     wakeup(b);
-
-
     if ((b = idequeue2) != 0) {
         idestart(b->dev, idequeue2);
         idequeue2 = b->qnext;
     }
-
-
-
-
     release(&idelock2);
-
 }
 
 //PAGEBREAK!
